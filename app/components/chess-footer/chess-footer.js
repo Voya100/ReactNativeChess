9b85a48429ac53bd991a40489531e3784fb6925b
Voya <voya@voyacode.com>
2017-05-24 22:54:21 +0300

    Add navigation buttons (initial)

--- app/components/chess-footer/chess-footer.js
+++ app/components/chess-footer/chess-footer.js
@@ -1,20 +1,45 @@
 import React, { Component } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Button } from 'react-native';
 
 import { ChessText } from '../shared/chess-text';
 
 export class ChessFooter extends Component {
   render() {
     return (
-      <View style={this.props.style}>
-        <ChessText>
-          Footer
-        </ChessText>
+      <View style={[this.props.style, styles.buttonContainer]}>
+        <View style={styles.buttonRow}>
+          <ChessNavButton title='New game'/>
+          <ChessNavButton title='Game mode'/>
+          <ChessNavButton title='Settings'/>
+        </View>
+        <View style={styles.buttonRow}>
+          <ChessNavButton title='Statistics'/>
+          <ChessNavButton title='Instructions'/>
+        </View>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
 
+const styles = StyleSheet.create({
+  buttonContainer: {
+    justifyContent: 'space-around'
+  },
+  buttonRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-around'
+  },
+  button: {
+    width: 110,
+    minWidth: 110,
+    flex: 1
+  }
 });
+
+// TODO: replace with touchable highlight
+class ChessNavButton extends Component{
+  render(){
+    return <View style={styles.button}><Button title={this.props.title} onPress={() => {}} /></View>
+  }
+}
