44a08937c09da7dbdf2d8b58336055581a5bcbe9
Voya <voya@voyacode.com>
2017-05-24 22:54:47 +0300

    Reorganize code in ChessMain

--- app/components/chess-main/chess-main.js
+++ app/components/chess-main/chess-main.js
@@ -3,20 +3,20 @@ import { StyleSheet, View, Dimensions } from 'react-native';
 
 import { ChessBoard } from './chess-board/chess-board'
 
-
 const border = 3;
 const margin = 2;
 
 export class ChessMain extends Component {
   render() {
     let fullBoardSize = Dimensions.get('window').width - 2*margin;
+    let boardSize = fullBoardSize - 2*border;
+    let tileSize = boardSize / 8;
+
     let mainStyle = {
       width: fullBoardSize,
       height: fullBoardSize
     }
-    // Without border
-    let boardSize = fullBoardSize - 2*border;
-    let tileSize = boardSize / 8;
+
     return (
       <View style={[styles.main, mainStyle]}>
           <ChessBoard game={this.props.game} tiles={this.props.tiles} pieces={this.props.pieces} tileSize={tileSize}></ChessBoard>
