7bd28113b665902a5bbe997219fa40e1e9ba2492
Voya <voya@voyacode.com>
2017-05-27 12:27:23 +0300

    Add CenteredModal as shared component (from ChooseModeModal)

--- app/components/screens/chess-main/choose-mode-modal/choose-mode-modal.js
+++ app/components/screens/chess-main/choose-mode-modal/choose-mode-modal.js
@@ -4,6 +4,8 @@ import i18n from 'react-native-i18n';
 
 import { ChessButton } from '../../../shared/chess-button';
 import { ChessText } from '../../../shared/chess-text';
+import { CenteredModal } from '../../../shared/centered-modal';
+
 
 import { SettingsActions } from '../../../../stores/settings-store';
 import { RoundStateStore } from '../../../../stores/round-state-store';
@@ -25,38 +27,18 @@ export class ChooseModeModal extends Component {
   render() {
     // Touchables close modal when pressed outside of the content area
     return (
-      <Modal animationType={"fade"} transparent={true} onRequestClose={this.props.onRequestClose} visible={this.props.visible}>
-        <TouchableWithoutFeedback onPress={this.props.onRequestClose}>
-          <View style={styles.container}>
-            <TouchableWithoutFeedback>
-              <View style={[this.props.style, styles.modalContent]}>
-                <ChessText style={styles.text}>{i18n.t('game.gameMode.title')}</ChessText>
-                <ChessButton style={styles.button} onPress={() => this.setMode(0)}>{i18n.t('game.gameMode.playerVsComputer')}</ChessButton>
-                <ChessButton style={styles.button} onPress={() => this.setMode(1)}>{i18n.t('game.gameMode.localMultiplayer')}</ChessButton>
-                <ChessButton style={styles.button} onPress={() => this.setMode(2)}>{i18n.t('game.gameMode.computerVsComputer')}</ChessButton>
-              </View>
-            </TouchableWithoutFeedback>
-          </View>
-        </TouchableWithoutFeedback>
-      </Modal>
+      <CenteredModal onRequestClose={this.props.onRequestClose} visible={this.props.visible}>
+        <ChessText style={styles.text}>{i18n.t('game.gameMode.title')}</ChessText>
+        <ChessButton style={styles.button} onPress={() => this.setMode(0)}>{i18n.t('game.gameMode.playerVsComputer')}</ChessButton>
+        <ChessButton style={styles.button} onPress={() => this.setMode(1)}>{i18n.t('game.gameMode.localMultiplayer')}</ChessButton>
+        <ChessButton style={styles.button} onPress={() => this.setMode(2)}>{i18n.t('game.gameMode.computerVsComputer')}</ChessButton>
+      </CenteredModal>
     );
   }
 }
 
 
 const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    flex: 1
-  },
-  modalContent: {
-    backgroundColor: colors.background,
-    borderWidth: 2,
-    borderRadius: 15,
-    margin: 10,
-    padding: 10
-  },
   text: {
     fontSize: 24,
     textAlign: 'center'
---
+++ app/components/shared/centered-modal.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { StyleSheet, View, Modal, TouchableWithoutFeedback } from 'react-native';
+import i18n from 'react-native-i18n';
+
+
+import { colors } from '../colors';
+
+export class CenteredModal extends Component {
+
+  render() {
+    // Touchables close modal when pressed outside of the content area
+    return (
+      <Modal animationType={"fade"} transparent={true} onRequestClose={this.props.onRequestClose} visible={this.props.visible}>
+        <TouchableWithoutFeedback onPress={this.props.onRequestClose}>
+          <View style={styles.container}>
+            <TouchableWithoutFeedback>
+              <View style={[styles.modalContent,this.props.style]}>
+                {this.props.children}
+              </View>
+            </TouchableWithoutFeedback>
+          </View>
+        </TouchableWithoutFeedback>
+      </Modal>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1
+  },
+  modalContent: {
+    backgroundColor: colors.background,
+    borderWidth: 2,
+    borderRadius: 15,
+    margin: 10,
+    padding: 10
+  }
+});
