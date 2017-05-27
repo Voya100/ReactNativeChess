b77e7091919f898a7234664909815de4ffd8c2f0
Voya <voya@voyacode.com>
2017-05-27 23:43:49 +0300

    Add game speed and max rounds settings to Settings page

--- app/components/screens/chess-settings/chess-settings.js
+++ app/components/screens/chess-settings/chess-settings.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Reflux from 'reflux';
 import i18n from 'react-native-i18n';
-import { StyleSheet, View, Picker } from 'react-native';
+import { StyleSheet, View, Picker, Slider, Switch } from 'react-native';
 
 import { SettingsStore, SettingsActions } from '../../../stores/settings-store';
 
 import { ChessText } from '../../shared/chess-text';
 import { ChessHeader } from '../../shared/chess-header';
+import { SliderWithNumber } from '../../shared/slider-with-number';
 
 export class ChessSettings extends Reflux.Component {
   static navigationOptions = {
@@ -16,41 +17,134 @@ export class ChessSettings extends Reflux.Component {
   constructor(){
     super();
     this.store = SettingsStore;
+		this.updateGameSpeed = this.updateGameSpeed.bind(this);
+		this.updateTempSpeed = this.updateTempSpeed.bind(this);
+		this.updateMaxRounds = this.updateMaxRounds.bind(this);
+		this.updateTempMaxRounds = this.updateTempMaxRounds.bind(this);
+		this.state = {tempSpeed: SettingsStore.state.gameSpeed, tempMaxRounds: SettingsStore.state.maxRounds};
   }
 
-  renderLanguageOptions(){
-    return Object.keys(i18n.translations).map((language, i) => {
-      return <Picker.Item label={ i18n.translations[language].id } value={ language } key={i}/> 
-    })
-  }
+	// tempSpeed and tempMaxRounds need to be initialized to right value after settings are fetchhed from AsyncStorage
+	componentDidUpdate(prevProps, prevState){
+		if(prevState.gameSpeed != this.state.gameSpeed){
+			this.setState({tempSpeed: this.state.gameSpeed});
+		}
+		if(prevState.maxRounds != this.state.maxRounds){
+			this.setState({tempMaxRounds: this.state.maxRounds});
+		}
+	}
 
 	updateLanguage(language){
 		console.log(language);
 		SettingsActions.setLanguage(language);
 		// A trick to make nav bar update
 		this.props.navigation.setParams({language});
+		this.saveSettings();
+	}
+
+	updateTempSpeed(tempSpeed){
+		this.setState({tempSpeed});
+	}
+
+	updateGameSpeed(speed){
+		SettingsActions.setGameSpeed(speed);
+	}
+
+	updateTempMaxRounds(tempMaxRounds){
+		this.setState({tempMaxRounds});
+	}
+
+	updateMaxRounds(rounds){
+		SettingsActions.setMaxRounds(rounds);
+	}
+
+	updateBoardReversed(reversed){
+		SettingsActions.setBoardReversed(reversed);
 	}
 
   render() {
-
     return (
-      <View>
+      <View style={styles.container}>
         <ChessHeader style={styles.header}>{i18n.t('settings.generalSettings')}</ChessHeader>
-				<View style={styles.optionContainer}>
-					<ChessText style={styles.text}>{i18n.t('settings.language')}</ChessText>
-					<Picker style={styles.languagePicker} 
-									selectedValue={this.state.language} 
-									onValueChange={language => this.updateLanguage(language)}
-									prompt={i18n.t('settings.selectLanguage')}> 
-						{this.renderLanguageOptions()}
-					</Picker>
-				</View>
-      </View>
+				{this.renderLanguagePicker()}
+				{this.renderGameSpeedSlider()}
+				{this.renderMaxRoundsSlider()}
+			</View>
     );
   }
+
+	renderLanguagePicker(){		
+		let RowContainer = this.renderRowContainer;
+		return (
+			<RowContainer title={i18n.t('settings.language')}>
+				<Picker style={styles.languagePicker} 
+								selectedValue={this.state.language} 
+								onValueChange={language => this.updateLanguage(language)}
+								prompt={i18n.t('settings.selectLanguage')}> 
+					{this.renderLanguageOptions()}
+				</Picker>
+			</RowContainer>
+		);
+	}
+	
+	renderRowContainer(props){
+		return (
+			<View style={styles.optionContainer}>
+				<ChessText style={styles.text}>{props.title}</ChessText>
+				{props.children}
+			</View>
+		)
+	}
+
+  renderLanguageOptions(){
+    return Object.keys(i18n.translations).map((language, i) => {
+      return <Picker.Item label={ i18n.translations[language].id } value={ language } key={i}/> 
+    });
+  }
+
+	renderGameSpeedSlider(){
+		let RowContainer = this.renderRowContainer;
+		return (
+			<RowContainer title={'Game speed'}>
+				<SliderWithNumber
+					step={1} 
+					minimumValue={1} 
+					maximumValue={5} 
+					value={this.state.gameSpeed} 
+					tempValue={this.state.tempSpeed}
+					onSlidingComplete={this.updateGameSpeed}
+					onValueChange={this.updateTempSpeed}
+				/>
+			</RowContainer>
+		)
+	}
+
+	renderMaxRoundsSlider(){
+		let RowContainer = this.renderRowContainer;
+		return (
+			<RowContainer title={'Max rounds'}>
+				<SliderWithNumber
+					step={50}
+					minimumValue={50}
+					maximumValue={500}
+					value={this.state.maxRounds}
+					tempValue={this.state.tempMaxRounds}
+					onSlidingComplete={this.updateMaxRounds}
+					onValueChange={this.updateTempMaxRounds}
+				/>
+			</RowContainer>
+		);
+	}
+
+	renderBoardReversedSwitch(){
+
+	}
 }
 
 const styles = StyleSheet.create({
+	container: {
+		margin: 10
+	},
 	optionContainer: {
 		flexDirection: 'row',
 		alignItems: 'center',
@@ -58,15 +152,14 @@ const styles = StyleSheet.create({
 		borderBottomWidth: 1
 	},
 	header: {
- 		margin: 10
+ 		margin:5
 	},
 	text: {
 		flex: 1,
-		fontSize: 18,
-		margin: 10
+		margin: 5
 	},
 	languagePicker: {
-		flex: 1,
+		flex: 2,
 		width: 100
 	}
 });
