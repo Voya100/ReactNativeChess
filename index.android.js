46f4c485d19205effeb1c7267f20437c6ca6b0d9
Voya <voya@voyacode.com>
2017-05-28 14:52:39 +0300

    Activat splashscreen always on startup

--- index.android.js
+++ index.android.js
@@ -1,7 +1,10 @@
 import { AppRegistry, UIManager } from 'react-native';
 
 import ReactNativeChess from './app/components/app';
+import SplashScreen from 'react-native-splash-screen';
 
 UIManager.setLayoutAnimationEnabledExperimental(true);
+
+SplashScreen.show();
 
 AppRegistry.registerComponent('ReactNativeChess', () => ReactNativeChess);
