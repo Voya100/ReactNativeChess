7775dc6813279ce5a629652d8db91d4aa26c3453
Voya <voya@voyacode.com>
2017-06-04 14:55:10 +0300

    Add hover color to drop zones in custom board settings

--- app/components/screens/chess-settings/custom-board-setup/tile-drop-zone.js
+++ app/components/screens/chess-settings/custom-board-setup/tile-drop-zone.js
@@ -8,17 +8,39 @@ import { DraggablePiece } from './draggable-piece';
 import { colors } from '../../../colors';
 
 export class TileDropZone extends Component {
+
+  constructor(){
+    super();
+    this.state = {isEntered: false}
+    this.onEnter = this.onEnter.bind(this);
+    this.onLeave = this.onLeave.bind(this);
+  }
+
+  onEnter(){
+    this.setState({isEntered: true});
+  }
+
+  onLeave(){
+    this.setState({isEntered: false});
+  }
   
   render(){
     let size = {width: this.props.size, height: this.props.size};
-    let color = (this.props.data.x + this.props.data.y) % 2 == 0 ? colors.blackTile : colors.whiteTile;
     return (
-      <DropZone onDrop={this.props.onDrop}>
-        <View style={[styles.tile, {backgroundColor: color}, size]}>
+      <DropZone onEnter={this.onEnter} onLeave={this.onLeave} onDrop={this.props.onDrop}>
+        <View style={[styles.tile, {backgroundColor: this.color()}, size]}>
           {this.props.data.pieceType && <DraggablePiece data={this.props.data} size={this.props.size}/>}
         </View>
       </DropZone>
     )
+  }
+
+  color(){
+    if(this.state.isEntered && this.props.data.isOnBoard){
+      return colors.active;
+    }else{
+      return (this.props.data.x + this.props.data.y) % 2 == 0 ? colors.blackTile : colors.whiteTile;
+    }
   }
 }
 
