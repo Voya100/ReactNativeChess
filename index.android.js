0d41cfd1926a1200f1abb82907c356e05df2bad4
Voya <voya@voyacode.com>
2017-06-04 21:11:02 +0300

    Remove splash screen import from index.android.js

--- index.android.js
+++ index.android.js
@@ -1,10 +1,8 @@
 import { AppRegistry, UIManager } from 'react-native';
 
 import ReactNativeChess from './app/components/app';
-import SplashScreen from 'react-native-splash-screen';
 
 UIManager.setLayoutAnimationEnabledExperimental(true);
 
-//SplashScreen.show();
 
 AppRegistry.registerComponent('ReactNativeChess', () => ReactNativeChess);
